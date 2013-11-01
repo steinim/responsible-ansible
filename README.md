71d4702e8bc6a38873fad7ec3077ffb131e272b2
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-11-01 13:51:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Install Ansible
+=============
+Mac:
+```brew install ansible```
+```sudo easy_install pip```
+```sudo pip install paramiko PyYAML jinja2```
+
+Other:
+http://www.ansibleworks.com/docs/intro_installation.html
+
+More:
+http://www.ansibleworks.com/docs/intro_getting_started.html
+
+Deploy with Ansible
+==================
+Go to ansible directory.
+Run: ```ansible-playbook --ask-sudo-pass deploy.yml```
+With parameters: ```ansible-playbook --ask-sudo-pass deploy.yml --extra-var="env=test version=0.1.0-SNAPSHOT"```
+
+Provision with Ansible
+=======================
+Go to ansible directory.
+Run: ```ansible-playbook --ask-sudo-pass provision.yml```
---
+++ roles/deploy/files/init.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+ENV=$( basename ${0} | cut -d\_ -f1 )
+APP=$( basename ${0} | cut -d\_ -f2 )
+APP_HOME=/home/appuser/backend/${ENV}
+STARTUP_SCRIPT=${APP_HOME}/current/bin/devops-${APP}
+LOGS=/var/logs/${APP}
+CURRENT=$( readlink ${APP_HOME}/current )
+GREP_PATTERN="\-Denv=${ENV} \-Dsecrets=${APP_HOME}/secret.conf"
+PID=$( ps -ea -o "pid ppid args" | grep -v grep | grep "${GREP_PATTERN}" | sed -e 's/^  *//' -e 's/ .*//' | head -1 )
+PORT=$( ps -ea -o "args" | grep "${GREP_PATTERN}" | ps -ea -o "args" | grep "${GREP_PATTERN}" | sed 's/.*-Dport=\([1-9]...\).*/\1/' | head -1 )
+
+if [ -n "${PORT}" ] ; then
+  [ ${PORT} -eq 7002 ] && [ "${ENV}" = "prod" ] && NEW_PORT=7003
+  [ ${PORT} -eq 7003 ] && [ "${ENV}" = "prod" ] && NEW_PORT=7002
+  [ ${PORT} -eq 7004 ] && [ "${ENV}" = "test" ] && NEW_PORT=7005
+  [ ${PORT} -eq 7005 ] && [ "${ENV}" = "test" ] && NEW_PORT=7004
+else
+  [ "${ENV}" = "test" ] && NEW_PORT=7004 || NEW_PORT=7002
+fi
+
+PARAMS="-mem 256 -Dport=${NEW_PORT} -Denv=${ENV} -Dsecrets=${APP_HOME}/secret.conf > ${APP_HOME}/current/myapp.out 2>&1 &"
+
+case "${1}" in
+  start)
+    NUMBER_OF_PROCS=$( ps -ef | grep "${GREP_PATTERN}" | wc -l )
+    [ ${NUMBER_OF_PROCS} ] && [ ${NUMBER_OF_PROCS} -gt 1 ] && echo "More than one process running. Exiting..." && exit 1
+    echo "Starting ${ENV} ${APP} on port ${NEW_PORT}..."
+    ${STARTUP_SCRIPT} ${PARAMS} 1>$LOGS/${ENV}_stdout.log 2>$LOGS/${ENV}_stderr.log &
+    NEW_PID=$!
+    ONLINE=""
+    while ( test -z "${ONLINE}" )
+    do
+      echo "Waiting for ${ENV} ${APP} to be available on port ${NEW_PORT}"
+      ONLINE=$( lsof -i:${NEW_PORT} | grep TCP )
+      sleep 1
+    done
+    echo "Started ${ENV} ${APP} on port ${NEW_PORT} (PID=${NEW_PID})"
+    ;;
+  stop)
+    echo "Stopping ${APP}..."
+    if [ -z "${PID}" ]; then
+      echo "${ENV} ${APP} is already stopped"
+    else
+      kill ${PID}
+      echo "Killed ${ENV} ${APP} on port ${PORT} (PID=${PID})."
+    fi
+    ;;
+  status)
+    PIDS=$( ps -ea -o "pid ppid args" | grep -v grep | grep "${GREP_PATTERN}" | sed -e 's/^  *//' -e 's/ .*//' )
+    test -n "${PIDS}" && echo "${ENV} ${APP} is running (${PIDS})." || echo "${ENV} ${APP} is not running."
+    ;;
+  restart)
+    $0 start
+    $0 stop
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    ;;
+esac
